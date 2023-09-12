cfdf2f0ebb62c4c46397844a1f0f053f6ee8a277
Jefferson Oliveira <jefferoidtex7@gmail.com>
2023-09-12 17:11:16 -0300

    fix: remove cache blade

--- storage/views/3e39b76f63b34f6f9bb61aeeffff14c6970ba19a.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <h2>Header</h2>
-</header>
-<?php /**PATH /var/www/html/wp-content/plugins/setup-plugin-gutenberg/src/resources/views/sections/header.blade.php ENDPATH**/ ?>
--- storage/views/e131f9328b6173a1cced55a18f40154fea25473b.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <h2>Footer</h2>
-</footer>
-<?php /**PATH /var/www/html/wp-content/plugins/setup-plugin-gutenberg/src/resources/views/sections/footer.blade.php ENDPATH**/ ?>
