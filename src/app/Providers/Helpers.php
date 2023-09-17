c000a4001d79e0609e29ef2272c3ad49b06d4da0
Jefferson Oliveira <jefferoidtex7@gmail.com>
2023-09-17 02:39:55 -0300

    feat: remove helpers não utilizado

--- src/app/Providers/Helpers.php
+++ src/app/Providers/Helpers.php
@@ -4,21 +4,4 @@ namespace App\Providers;
 
 class Helpers
 {
-    public static function templateRenderer($filePath, $variables = [])
-    {
-        // Extract the variables to a local namespace
-        extract($variables);
-
-        // Start output buffering
-        ob_start();
-
-        // Include the template file
-        include PLUGIN_SETUP_GUTENBERG_VIEWS_PATH . $filePath . '.php';
-
-
-        // End buffering and return its contents
-        $output = ob_get_clean();
-
-        return $output;
-    }
 }
